57ddf6440a95a74d989a9a34e9e75344eb8437d5
seetharam9999 <undamatlakodandaseetharam1@gmail.com>
2026-03-25 09:23:43 +0530

    Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,22 @@
+/**
+ * The BookingApplication class serves as the entry point for the
+ * Hotel Booking System. It initializes the system and displays
+ * versioning information to the user.
+ * * @author User
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    /**
+     * The main method is the first method called by the JVM.
+     * * @param args Command-line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        // Displaying the welcome message and application metadata
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Application Name: StaySafe Booker");
+        System.out.println("Version: 1.0.0-SNAPSHOT");
+
+        // The program terminates naturally after this line
+    }
+}
